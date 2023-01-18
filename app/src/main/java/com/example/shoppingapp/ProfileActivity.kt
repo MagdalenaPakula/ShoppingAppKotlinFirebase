e5e295c13803592405a9b532c68f47095e70a855
Magdalena Pakuła <magdalena.pakula001@gmail.com>
2023-01-18 23:20:48 +0100

    Adding all files so now: - logging in, - registering, - connecting to Firebase authorisation - connecting to Firebase database

---
+++ app/src/main/java/com/example/shoppingapp/ProfileActivity.kt
@@ -0,0 +1,63 @@
+package com.example.shoppingapp
+
+import android.app.ProgressDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.example.shoppingapp.databinding.ActivityProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class ProfileActivity : AppCompatActivity() {
+
+    //viewBinding
+    private lateinit var binding: ActivityProfileBinding
+    //ActionBar
+    private lateinit var actionBar: ActionBar
+    //Firebase auth
+    //private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //configure actionbar
+        actionBar = supportActionBar!!
+        actionBar.title = "What's on Your mind today?"
+
+
+
+
+
+
+//        //init firebase auth
+//        firebaseAuth = FirebaseAuth.getInstance()
+//        checkUser()
+//
+//        //handle logout
+//        binding.logoutBtn.setOnClickListener{
+//            firebaseAuth.signOut()
+//            checkUser()
+//        }
+    }
+
+
+
+
+
+
+//    private fun checkUser(){
+//        //check if user is logged in
+//        val firebaseUser = firebaseAuth.currentUser
+//        if(firebaseUser != null){
+//            //ok
+//            val email = firebaseUser.email
+//            binding.emailTv.text = email
+//        }else {
+//            //not ok
+//            startActivity(Intent(this, LoginActivity::class.java))
+//            finish()
+//        }
+//    }
+}
---
+++ app/src/main/java/com/example/shoppingapp/RegisterActivity.kt
@@ -0,0 +1,105 @@
+package com.example.shoppingapp
+
+import android.app.ProgressDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import com.example.shoppingapp.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class RegisterActivity : AppCompatActivity() {
+
+    //ViewBinding
+    private lateinit var binding: ActivityRegisterBinding
+    //ActionBar
+    private lateinit var actionBar: ActionBar
+    //ProgressDialog
+    private lateinit var progressDialog: ProgressDialog
+    //Firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private var email = ""
+    private var password = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //configure actionbar & enable back button
+        actionBar = supportActionBar!!
+        actionBar.title = "Sign up"
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.setDisplayShowHomeEnabled(true)
+
+        //configure progress dialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setMessage("Creating account...")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        //handle click, begin signing up
+        binding.signUpBtn.setOnClickListener{
+            validateData()
+        }
+    }
+
+    private fun validateData(){
+        // get data
+        email = binding.emailEt.text.toString().trim()
+        password = binding.passwordEt.text.toString().trim()
+
+        //validate data
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            //invaild email format
+            binding.emailEt.error = "Invalid email format"
+        }
+        else if (TextUtils.isEmpty(password)){
+            //no pass entered
+            binding.passwordEt.error = "Please enter password"
+        }
+        else if (password.length < 8){
+            //pass < 8
+            binding.passwordEt.error = "Password must be at least 8 characters long"
+        }
+        else{
+            //data correct
+            firebaseRegister()
+        }
+    }
+
+    private fun firebaseRegister(){
+        //show progress
+        progressDialog.show()
+
+        //create account
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener {
+            //login success
+            progressDialog.dismiss()
+            //get user info
+            val firebaseUser = firebaseAuth.currentUser
+            val email = firebaseUser!!.email
+            Toast.makeText(this, "Account has been created with email $email", Toast.LENGTH_SHORT).show()
+
+            //open profile
+            startActivity(Intent(this, ProfileActivity::class.java))
+            finish()
+        }.addOnFailureListener { e ->
+            //login failed
+            progressDialog.dismiss()
+            Toast.makeText(this, "Login failed due ${e.message}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed() //go back to previous activity when back button on action bar clicked
+        return super.onSupportNavigateUp()
+    }
+}
